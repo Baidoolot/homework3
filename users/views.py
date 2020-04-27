142adbec8c1a8fd85d5a5fb3778bdf0786d6f802
Baidoolot <bay.sonunbek@gmail.com>
2020-04-28 01:37:45 +0600

    home work

---
+++ users/views.py
@@ -0,0 +1,48 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, ProfileForm
+from .decorators import unauthenticated_user
+
+
+
+@unauthenticated_user
+def register_page(request):
+    form = CreateUserForm
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
+
+
+@unauthenticated_user
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('post')
+    context = {}
+    return render(request, 'users/login.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('post')
+
+@login_required(login_url='login')
+def profile(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return render(request, 'users/profile.html', context)
